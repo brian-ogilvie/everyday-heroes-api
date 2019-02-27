048b0016b6ea0922305ceb4405aa782e3c6a63df
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-27 13:55:26 -0500

    error handling for assignments routes

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,9 +1,18 @@
 class AssignmentsController < ApplicationController
   def create
-    assignment = Assignment.new(assignment_params)
-    assignment[:user_id] = current_user[:id]
-    assignment.save
-    render json: {completed_assignment: assignment}, status: 201
+    begin
+      assignment = Assignment.new(assignment_params)
+      assignment[:user_id] = current_user[:id]
+      if assignment.save
+        render json: {completed_assignment: assignment}, status: 201
+      else 
+        render json: {error: assignment.errors.full_messages}, status: 400
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: {error: "No task found with that id."}, status: 404
+    rescue Exception
+      render json: {error: "Internal Server Error"}, status: 500
+    end
   end
 
   private
