b3fbe1b78a23fad90e3f798802a93d63c330cf43
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-01 11:27:48 -0500

    added route to check for tasks already completed today

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -15,6 +15,23 @@ class AssignmentsController < ApplicationController
     end
   end
 
+  def completed_assignment_by_habit
+    begin
+      completed = (Assignment.joins(:daily_task)
+        .select("daily_tasks.*")
+        .where(
+          assignments: {user_id: current_user[:id], created_at: (Time.now.midnight..Time.now)}, 
+          daily_tasks: {level_id: current_user[:level_id], heroic_habit_id: params[:habit_id]}
+        )
+      ).first
+      render json: {completed: completed}, status: 200
+    rescue ActiveRecord::RecordNotFound
+      render json: {message: 'No completed tasks found.'}
+    rescue Exception
+      render json: {errors: ["Internal Server Error"]}, status: 500
+    end
+  end
+
   private
   def assignment_params
     params.require(:assignment).permit(:daily_task_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'application#index'
   
-  post 'assignments/', to: 'assignments#create'
+  get '/today/assignments/:habit_id', to: 'assignments#completed_assignment_by_habit'
+  post '/assignments/', to: 'assignments#create'
 
   get '/today', to: 'tasks#today'
   get '/today/:habit_id', to: 'tasks#tasks_for_habit'
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
 
   resources :users
   get '/users/:id/progress', to: 'users#progress'
-  post 'authenticate', to: 'authentication#authenticate'
+  post '/authenticate', to: 'authentication#authenticate'
 end
