4642a5e1d65b6d058703a60402f402b7961d803a
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-04 02:34:02 -0500

    added ENV secret key variable to encode

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,8 +1,9 @@
 class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
+      secret_key_base = ENV['SECRET_KEY_BASE'] || Rails.application.credentials.secret_key_base
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.credentials.secret_key_base)
+      JWT.encode(payload, secret_key_base)
     end
 
     def decode(token)
