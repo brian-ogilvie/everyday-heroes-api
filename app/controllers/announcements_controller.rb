53707a4e3fd884f8a2729568871279208c9185a9
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-05 16:10:08 -0500

    announcements are marked as delivered after they have been sent

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -6,10 +6,22 @@ class AnnouncementsController < ApplicationController
         a.as_json()
       end
       render json: {announcements: json_announcements}
+      mark_as_delivered announcements
     rescue ActiveRecord::RecordNotFound
       render json: {error: "Record Not Found"}, status: 404
-    rescue Exception
-      render json: {error: "Internal Server Error"}, status: 500
+    # rescue Exception
+    #   render json: {error: "Internal Server Error"}, status: 500
+    end
+  end
+
+  private
+
+  def mark_as_delivered(announcements)
+    announcements.each do |announcement|
+      entry = announcement.user_announcements.where({user_id: current_user[:id]}).first
+      if entry
+        entry.update_attributes({delivered: true})
+      end
     end
   end
 end
