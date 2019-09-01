6191ac09ca5cb025f17c800b5cdd55c59a6b01fe
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-09-01 19:39:09 -0400

    allow cors access for new front end site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://everyday-heroes.herokuapp.com'
+    origins 'https://everyday-heroes.netlify.com'
 
     resource '*',
       headers: :any,
